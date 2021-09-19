dc56e635a854e14f071cdd93ff090ccccc7932c9
HusseinnAlshaikh <86023998+HusseinnAlshaikh@users.noreply.github.com>
2021-09-19 21:36:16 +0300

    Delete add-card.tsx

--- add-card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AddCard({handleAddCard}: any) {
-    return (
-        <div className="add-card" onClick={handleAddCard}>
-            + add a card
-        </div>
-    )
-}
